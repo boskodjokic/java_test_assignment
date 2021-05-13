2ae0c22a71f916542256a49d5f27d5f1b472603e
boskodjokic <boskodjokic@yahoo.com>
2021-05-13 16:44:41 +0200

    Adding TeachersTab and TeachersTabTest.

--- src/main/java/com/alasdoo/developercourseassignment/e2e/web/TeachersTab.java
+++ src/main/java/com/alasdoo/developercourseassignment/e2e/web/TeachersTab.java
@@ -31,6 +31,9 @@ public class TeachersTab {
     @FindBy(css = "button[data-test-id='save']")
     private WebElement teacherSaveButton;
 
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/main[1]/div[2]/div[3]/div[1]/form[1]/div[2]/button[1]")
+    private WebElement teacherSaveCourseButton;
+
     @FindBy(css = "button[data-test-id='delete']")
     private WebElement teacherDeleteButton;
 
@@ -58,6 +61,20 @@ public class TeachersTab {
     @FindBy(xpath = "//body/div[@id='menu-developerCourseId']/div[3]/ul[1]")
     private WebElement allCourses;
 
+    @FindBy(css = "div.makeStyles-root-1 main.makeStyles-content-3 div.makeStyles-mainContent-4 div.MuiDataGrid-root.MuiDataGrid-root div.MuiDataGrid-mainGridContainer:nth-child(2) div.MuiDataGrid-window div.MuiDataGrid-dataContainer.data-container div.MuiDataGrid-viewport div.rendering-zone div.MuiDataGrid-row.Mui-even > div.MuiDataGrid-cell.MuiDataGrid-cellLeft:nth-child(2)")
+    private WebElement nameInTable;
+
+    @FindBy(css = "button[data-test-id='add-courses']")
+    private WebElement addCourseButton;
+
+    @FindBy(xpath = "//body/div[@id='root']/div[1]/main[1]/div[2]/div[3]/div[2]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]")
+    private WebElement coursesTable;
+
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/main[1]/div[2]/div[3]/div[2]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]")
+    private WebElement emptyCoursesTable;
+
+//    @FindBy(xpath = "//body/div[@id='root']/div[1]/main[1]/div[2]/div[3]/div[2]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]")
+//    private WebElement emptyCoursesTable;
 
     //    Method for entering teachers name
     public void enterName(String name) {
@@ -77,6 +94,11 @@ public class TeachersTab {
     //    Method for clicking save button
     public void clickSaveButton(){
         teacherSaveButton.click();
+    }
+
+    //    Method for clicking save courses button
+    public void clickSaveCourseButton(){
+        teacherSaveCourseButton.click();
     }
 
     //    Method for clicking delete button
@@ -123,20 +145,63 @@ public class TeachersTab {
     }
 
     //    Method for clicking cancel course button
-    public void clickOnDCancelCourseButton() {
+    public void clickOnCancelCourseButton() {
         cancelCourseButton.click();
+    }
+
+    //    Method for clicking add course button
+    public void clickOnAddCourseButton() {
+        addCourseButton.click();
+    }
+
+//    Method for asserting is the button displayed
+    public boolean isSaveButtonDisplayed() {
+        teacherSaveButton.isDisplayed();
+        return true;
+    }
+
+    //    Method for getting the text from the table
+    public String getNameFromTable() {
+        return nameInTable.getText();
+    }
+
+    //    Method for clicking newly added teacher account name in table
+    public void clickNameFromTable() {
+        nameInTable.click();
     }
 
     //    Clicking on a random course from a course list
     public String clickOnRandomCourse() {
-        List<WebElement> listOfCourses = allCourses.findElements(By.className("MuiTouchRipple-root"));
-        Random random = new Random(20);
-        int courseIndex = random.nextInt(20);
+        List<WebElement> listOfCourses = allCourses.findElements(By.tagName("li"));
+        Random random = new Random();
+        int courseIndex = random.nextInt(listOfCourses.size());
         Actions actions = new Actions(driver);
         String chosenCourse = listOfCourses.get(courseIndex).getText();
-        System.out.println(chosenCourse);
         actions.moveToElement(listOfCourses.get(courseIndex)).click().perform();
         return chosenCourse;
+    }
+
+    //Getting last course from view table
+    public String getLastCourseFromView() {
+        String chosenCourse = coursesTable.getText();
+        String[] courseSplit = chosenCourse.split("\\r?\\n");
+        return courseSplit[courseSplit.length - 1];
+    }
+
+    //    Getting text from courses table
+    public String getCoursesTable() {
+        return coursesTable.getText();
+    }
+
+//     Checking how many courses are in the table. It is divided by 2 as it takes course name and classes bought together from the viewport.
+    public int numberOfCoursesInTable() {
+        String[] check = getCoursesTable().split("\\r?\\n");
+        return check.length;
+    }
+
+    //    Getting text from empty courses table
+    public String getEmptyCoursesTableText() {
+        return emptyCoursesTable.getText();
     }
 
 }
--- src/test/java/com/alasdoo/developercourseassignment/e2e/web/TeachersTabTest.java
+++ src/test/java/com/alasdoo/developercourseassignment/e2e/web/TeachersTabTest.java
@@ -1,7 +1,150 @@
 package com.alasdoo.developercourseassignment.e2e.web;
 
+import com.alasdoo.developercourseassignment.e2e.common.BrowserFactory;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TeachersTabTest {
 
+    private MainMenu mainMenu;
+    private TeachersTab teachersTab;
+
+    WebDriver driver = BrowserFactory.getBrowser(System.getProperty("browser"));
+
+    @BeforeEach
+    void setup() {
+
+        driver.get("http://localhost:3000/teacher");
+        driver.manage().window().maximize();
+        mainMenu = new MainMenu(driver);
+        teachersTab = new TeachersTab(driver);
+    }
+
+    //    We are asserting is the save button displayed, because it should not be, when the teacher is successfully saved. Email is left out.
+    @Test
+    @Order(1)
+    void saveTeacherWithIncompleteData() {
+        mainMenu.clickPlusButton();
+        teachersTab.enterName("Bosko");
+        teachersTab.enterSurname("Myers");
+        teachersTab.clickSaveButton();
+        assertTrue(teachersTab.isSaveButtonDisplayed());
+    }
+
+    //    Test for creating new teacher and checking is the teacher in the table
+    @Test
+    @Order(2)
+    void teacherSuccessfullySaved() throws InterruptedException {
+        mainMenu.clickPlusButton();
+        teachersTab.enterName("Bosko");
+        teachersTab.enterSurname("Djokic");
+        teachersTab.enterEmail("bosko@gmail.com");
+        teachersTab.clickSaveButton();
+        int clicks = mainMenu.getNumberOfClicks();
+        for (int i = 0; i <= clicks - 1; i++) {
+            mainMenu.clickRightArrowButton();
+        }
+        assertEquals("Bosko", teachersTab.getNameFromTable());
+    }
+
+    //    Testing update of the teacher
+    @Test
+    @Order(3)
+    void updateTeacher() throws InterruptedException {
+        int clicks = mainMenu.getNumberOfClicks();
+        for (int i = 0; i <= clicks - 1; i++) {
+            mainMenu.clickRightArrowButton();
+        }
+        teachersTab.clickNameFromTable();
+        teachersTab.clearName();
+        teachersTab.enterName("Kosta");
+        teachersTab.clickSaveButton();
+
+        assertEquals("Kosta", teachersTab.getNameFromTable());
+    }
+
+    //    Testing deletion of the teacher
+    @Test
+    @Order(6)
+    void deleteTeacher() throws InterruptedException {
+        int clicks = mainMenu.getNumberOfClicks();
+        for (int i = 0; i <= clicks - 1; i++) {
+            mainMenu.clickRightArrowButton();
+        }
+        teachersTab.clickNameFromTable();
+        String name = teachersTab.getNameFromTable();
+        teachersTab.clickDeleteButton();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        WebElement nameFromTable = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]"));
+        assertNotEquals(name, nameFromTable.getText());
+
+    }
+
+    //    Testing addition of new course for the teacher
+    @Test
+    @Order(4)
+    void addCourse() throws InterruptedException {
+        int clicks = mainMenu.getNumberOfClicks();
+        for (int i = 0; i <= clicks - 1; i++) {
+            mainMenu.clickRightArrowButton();
+        }
+        teachersTab.clickNameFromTable();
+        teachersTab.clickToggleCoursesButton();
+        teachersTab.clickOnAddCourseButton();
+        teachersTab.clickOnCoursesDropdownList();
+        String course =  teachersTab.clickOnRandomCourse();
+        WebDriverWait wait=new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//body/div[@id='root']/div[1]/main[1]/div[2]/div[3]/div[1]/form[1]/div[2]/button[1]")));
+        Thread.sleep(3000);
+        teachersTab.clickSaveCourseButton();
+        teachersTab.clickXButton();
+
+        teachersTab.clickNameFromTable();
+        teachersTab.clickToggleCoursesButton();
+
+        String courseFromView = teachersTab.getLastCourseFromView();
+        assertEquals(course, courseFromView);
+
+    }
+
+    //    Testing deletion of the teachers course
+    @Test
+    @Order(5)
+    void deleteCourse() throws InterruptedException {
+        int clicks = mainMenu.getNumberOfClicks();
+        for (int i = 0; i <= clicks - 1; i++) {
+            mainMenu.clickRightArrowButton();
+        }
+        teachersTab.clickNameFromTable();
+        teachersTab.clickToggleCoursesButton();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        String courseFromView = teachersTab.getLastCourseFromView();
+        int numberOfCourses = teachersTab.numberOfCoursesInTable();
+        WebElement course = driver.findElement(By.xpath("//div[contains(text(),'" + courseFromView + "')]"));
+        course.click();
+        teachersTab.clickOnDeleteCourseButton();
+
+        if(numberOfCourses>1) {
+            assertNotEquals(courseFromView, teachersTab.getLastCourseFromView());
+        }
+        else {
+            assertNotEquals(courseFromView, teachersTab.getEmptyCoursesTableText());
+        }
+
+    }
+    @AfterAll
+    void close() {
+        driver.close();
+    }
 }
